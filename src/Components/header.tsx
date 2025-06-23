62d129377b388144dbfd70b646aa521b8f245a7d
João Pedro <jpsousa471@gmail.com>
2025-06-22 22:47:24 -0300

    atualizacoes

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header className="w-full h-17 max-w-636 bg-[#121114] text-white center">
            ola
-           <span className="">aaaa</span>
+           <span className=" left-5">aaaa</span>
            <span>kkkkk</span>
 
         </header>
--- src/page/Home/page.tsx
+++ src/page/Home/page.tsx
@@ -7,13 +7,13 @@ function Home() {
     return(
         <>
             <Header />
-            <div className="bg-[#141414]  w-full h-full center">
+            <div className="bg-[#141414]  w-full h-full gCenter">
 
-                <div className="bg-blue-600 max-w-325 h-125 mt-10">
+                <div className="bg-blue-600 max-w-320 h-125 mt-7 rounded-2xl">
                     65465465
                 </div>
 
-                <div className="bg-red-500 flex space-x-10 h-90 w-200 ">
+                <div className="bg-red-500 space-x-12 h-90 w-320 center mt-15 ">
                     <div>a</div>
                     <div>a</div>
                     <div>a</div>
